f334f07404e8738a696696e9b948a89bb088629b
Web-proger <guser999@mail.ru>
2017-01-15 19:39:32 +0700

    Оптимизация JS
    - Деделал показ анимации ошибки при повторном неправильном вводе.
    - Добавил комментарии к JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,7 +11,7 @@ var mapButtonFooter = document.querySelector(".js-footer-open-map");
 var map = document.querySelector(".modal-content-map");
 var mapClose = map.querySelector(".modal-content-close");
 
-
+//Открытие формы входа
 userBlock.addEventListener("click", function(event) {
 	event.preventDefault();
 	popup.classList.add("modal-content-show");
@@ -24,16 +24,22 @@ userBlock.addEventListener("click", function(event) {
 	}
 });
 
+// Закрытие формы входа по крестику
 popupClose.addEventListener("click", function(event) {
 	popup.classList.remove("modal-content-show");
 	popup.classList.remove("modal-form-error");
 	console.log("Закрываем Popup по крестику");
 });
 
+// Анимация ошибки входа, если незаполнено коно логина или пароля. 
+// Запоминание Логина
 form.addEventListener("submit", function(event) {
 	if (!login.value || !password.value) {
 		event.preventDefault();
-		popup.classList.add("modal-form-error");
+		popup.classList.remove("modal-form-error");
+		setTimeout(function() {
+		popup.classList.add("modal-form-error");	
+		}, 1)
 		console.log("Не введен логин или пароль")
 } else {
 	event.preventDefault();
@@ -41,14 +47,14 @@ form.addEventListener("submit", function(event) {
 	console.log("Отправляем форму");
 	console.log("Ваш логин:" + login.value);
 	console.log("Ваш пароль:" + password.value);
-};
+	};
 });
 
+// Закрытие формы входа по escape
 window.addEventListener("keydown", function(event) {
 	if (event.keyCode === 27) {
 		if (popup.classList.contains("modal-content-show")) {
 			popup.classList.remove("modal-content-show");
-			popup.classList.remove("modal-form-error");
 			console.log("Закрываем Popup через Escape");
 		} else {
 			console.log("Popup не открыт");
@@ -59,22 +65,17 @@ window.addEventListener("keydown", function(event) {
 		} else {
 			console.log("Map не открыт");
 		}
-
 	}
 });
 
-/*mapButton.addEventListener("click", function(event) {
-	event.preventDefault();
-	map.classList.add("open-map");
-	console.log("Открываем карту");
-});*/
-
+// Открытие карты
 mapButtonFooter.addEventListener("click", function(event) {
 	event.preventDefault();
 	map.classList.add("open-map");
 	console.log("Открываем карту");
 });
 
+// Закрытие карты
 mapClose.addEventListener("click", function(event) {
 	event.preventDefault();
 	map.classList.remove("open-map");
